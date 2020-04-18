f25a968edbb0a82671b3f01048facb88d31a10b0
vejjutejdeep <tej2544@msitprogram.net>
2020-04-19 01:44:50 +0530

    finalchangestablecreating

--- create.py
+++ create.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template, request
 from userdata import *
 
 app = Flask(__name__)
+os.environ["DATABASE_URL"] = "postgres://ymjvjqxehzwaub:aadaba6ce5d972237f60a1343ab28645884515fee52b1d9aea95ab421a14537c@ec2-18-210-51-239.compute-1.amazonaws.com:5432/dbuaf71hle517p"
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db.init_app(app)
--- userdata.py
+++ userdata.py
@@ -4,10 +4,10 @@ from datetime import datetime
 db = SQLAlchemy()
 
 class userdata(db.Model):
-    __tablename__=userdata
-    username=db.column(db.String, primary_key=True)
-    password=db.column(db.String, nullable=False)
-    time=db.column(db.datetime, nullable=False)
+    __tablename__="userdata"
+    username = db.Column(db.String, primary_key=True)
+    password = db.Column(db.String, nullable=False)
+    time = db.Column(db.DateTime, nullable=False)
 
     def _init_(self, username, password, time):
         self.username = username

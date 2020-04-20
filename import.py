8aa913bfda2c15912efbec0d51d40d64ef4de56f
vejjutejdeep <tej2544@msitprogram.net>
2020-04-20 18:29:39 +0530

    task3com

--- import.py
+++ import.py
@@ -1,16 +1,21 @@
+import csv
 import os
 
 from flask import Flask, render_template, request
 from model import *
 
 app = Flask(__name__)
-os.environ["DATABASE_URL"] = "postgres://ymjvjqxehzwaub:aadaba6ce5d972237f60a1343ab28645884515fee52b1d9aea95ab421a14537c@ec2-18-210-51-239.compute-1.amazonaws.com:5432/dbuaf71hle517p"
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db.init_app(app)
 
 def main():
-    db.create_all()
+    f = open("books.csv")
+    reader = csv.reader(f)
+    for number, name, author, year in reader:
+        book = books(number = number, name = name, author = author, year = year)
+        db.session.add(book)
+    db.session.commit()
 
 if __name__ == "__main__":
     with app.app_context():
---
+++ model.py
@@ -0,0 +1,20 @@
+import os
+
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class books(db.Model):
+    __tablename__="books"
+    ISBN_number = db.Column(db.String, primary_key = True)
+    name = db.Column(db.String, nullable = False, unique = True)
+    author = db.Column(db.String, nullable = False)
+    publication_year = db.Column(db.Integer, nullable = False)
+
+    def _init_(self, number, name, author, year):
+        self.ISBN_number = number
+        self.name = name
+        self.author = author
+        self.publication_year = year
